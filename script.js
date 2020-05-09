f043108a751c7268bc28d9436b29fb11a059839e
tlatta13 <tlatta1313@gmail.com>
2020-05-09 19:49:41 -0400

    Updated html and js files

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
 // Display current date and time
 var currentDateAndTime = Date(Date.now());
-    $("#currentDay").append(currentDateAndTime);
+$("#currentDay").append(currentDateAndTime);
 
 var currentHour = new Date().getHours();
+        console.log(currentHour);
 
 // Click function for save buttons  
-$(".btn").click(function() {
-    var hour = $(this).val()
-    localStorage.setItem("time" + hour, $("#textInput").val());
+$(".btn").click(function() { 
+    var hourNum = $(this).val();
+    localStorage.setItem("time" + hourNum, $("#textInput" + hourNum).val());
 })
 
+function checkHours() {
+    // Loop through hours
+    for (var i = 9; i < 18; i++) {
+        
+        // get local storage
+        var noteInput = localStorage.getItem("time" + i);
+        if(noteInput !== null) {
+            $(".description" + i).val(noteInput);
+            // console.log(noteInput)
+        }   
+        
+        // Change text area color
+        if (parseInt(currentHour) > i) {
+            $(".description").addClass("past");
+            $(".description").removeClass("present");
+            $(".description").removeClass("future");
+            // console.log(i)
+        } 
+        else if (parseInt(currentHour) === i) {
+            $(".description").addClass("present");
+            $(".description").removeClass("past");
+            $(".description").removeClass("future");
+            // console.log(i) 
+        } 
+        else { 
+            $(".description").addClass("future");
+            $(".description").removeClass("present");
+            $(".description").removeClass("past");
+            // console.log(i)
+        }
+        
+    }
+}
 
-// Loop through hours to change text area color
-for (var i = 9; i < 18; i++) {
-    var noteInput = localStorage.getItem("time" + i)
-    if(noteInput !== null) {
-        $("#textInput" + i).val(noteInput);
-        console.log(noteInput)
-    }       
-}        
+
+checkHours()
